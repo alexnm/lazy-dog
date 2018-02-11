a4b6ab9093b83edce62076a5f0f3b314dd67bdb6
alexnm <alex.n.moldovan@gmail.com>
2018-02-11 12:48:04 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const dev = process.env.NODE_ENV !== "production";
+const webpack = require( "webpack" );
+const path = require( "path" );
+const { BundleAnalyzerPlugin } = require( "webpack-bundle-analyzer" );
+
+const plugins = [
+    new webpack.optimize.CommonsChunkPlugin( {
+        name: "lib",
+        minChunks: Infinity,
+        filename: "[name].bundle.js",
+    } ),
+];
+
+if ( !dev ) {
+    plugins.push(
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.DefinePlugin( {
+            "process.env.NODE_ENV": JSON.stringify( "production" ),
+        } ),
+        new webpack.optimize.UglifyJsPlugin( { mangle: false, sourceMap: true } ),
+        new BundleAnalyzerPlugin( {
+            analyzerMode: "static",
+            reportFilename: "webpack-report.html",
+            openAnalyzer: false,
+        } ),
+    );
+}
+
+module.exports = {
+    context: path.join( __dirname, "src" ),
+    devtool: dev ? "none" : "source-map",
+    entry: {
+        app: "./js/App.js",
+        lib: [ "react", "react-dom" ],
+    },
+    resolve: {
+        modules: [
+            path.resolve( "./src" ),
+            "node_modules",
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+            },
+        ],
+    },
+    output: {
+        path: path.resolve( __dirname, "dist" ),
+        filename: "[name].bundle.js",
+    },
+    plugins,
+};
